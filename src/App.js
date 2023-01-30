dd479091b5ce1bcc3b276eb3523a430290eeee4a
Laurence Lewis (he/him) <laurence@a11ycats.com.au>
2023-01-30 10:33:49 +1000

    Remove redundant DIV Removed unnecessary DIV wrapper.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import DescriptionList from "./DescriptionList";
 
-function App() {
-  return (
-    <div>
-      <DescriptionList />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <DescriptionList />;
+  }
 }
 
 export default App;
